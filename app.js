ab6cdecb745c2eb53c1d7d20429a9c1fb10a2f59
ycarr5150 <ycarrillo@csumb.edu>
2019-10-26 20:50:18 +0000

    Updating lab6

--- app.js
+++ app.js
@@ -10,18 +10,18 @@ app.get("/", function(req, res) {
 });
 
 app.get("/mercury", function(req, res) {
-    res.send("This will be Mercury web page!"); 
+    res.render("mercury.html"); 
 });
 
 app.get("/venus", function(req, res) {
-    res.send("This will be Venus web page!"); 
+    res.render("venus.html"); 
 });
 
 // server listener 
-app.listen("8081", "127.0.0.1", function(){
-    console.log("Express Server is Running ..."); 
-}); 
+// app.listen("8081", "127.0.0.1", function(){
+//     console.log("Express Server is Running ..."); 
+// }); 
 
-// app.listen(process.end.PORT, process.env.IP, function() {
-//     console.log("Running Express Server..."); 
-// });
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Running Express Server..."); 
+});
