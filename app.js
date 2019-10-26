862d6ad7e40b26cfa3ce9e54534ab9eb9aebeb35
ycarr5150 <ycarrillo@csumb.edu>
2019-10-26 19:15:13 +0000

    Initial change

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express"); 
+const app = express(); 
+
+// routes
+app.get("/", function(req, res) {
+    //res.send("it works!"); 
+    res.render("index.html"); 
+});
+
+app.get("/mercury", function(req, res) {
+    res.send("This will be Mercury web page!"); 
+});
+
+app.get("/venus", function(req, res) {
+    res.send("This will be Venus web page!"); 
+});
+
+// server listener 
+app.listen("8081", "127.0.0.1", function(){
+    console.log("Express Server is Running ..."); 
+}); 
