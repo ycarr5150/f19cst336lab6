4f471956ad0bb18fab8927306ba63528fe1e007c
ycarr5150 <ycarrillo@csumb.edu>
2019-10-31 19:12:13 +0000

    updating lab6

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ app.use(express.static("public"));
 
 // routes
 app.get("/", function(req, res) {
-    //res.send("it works!"); 
     res.render("index.html"); 
 });
 
@@ -17,10 +16,22 @@ app.get("/venus", function(req, res) {
     res.render("venus.html"); 
 });
 
+app.get("/earth", function(req, res) {
+    res.render("earth.html"); 
+});
+
+app.get("/mars", function(req, res) {
+    res.render("mars.html"); 
+});
+
+app.get("/jupiter", function(req, res) {
+    res.render("jupiter.html"); 
+});
+
 // server listener 
-// app.listen("8081", "127.0.0.1", function(){
-//     console.log("Express Server is Running ..."); 
-// }); 
+app.listen("8081", "127.0.0.1", function(){
+    console.log("Express Server is Running ..."); 
+}); 
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Running Express Server..."); 
