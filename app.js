a210148f4de7a7c4fb35c77bd1b7d66c1317a094
ycarr5150 <ycarrillo@csumb.edu>
2019-11-05 02:57:20 +0000

    updating css file

--- app.js
+++ app.js
@@ -29,10 +29,6 @@ app.get("/jupiter", function(req, res) {
 });
 
 // server listener 
-app.listen("8081", "127.0.0.1", function(){
-    console.log("Express Server is Running ..."); 
-}); 
-
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("Running Express Server..."); 
 });
